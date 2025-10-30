b26ca5faa6b7abecd7718f2b7b91715d9f8c2f3b
Jahnavi Onteddu <jahnavi.o21@iiits.in>
2025-10-30 11:07:26 +0530

    Update var.tf

--- jobportal-infra/var.tf
+++ jobportal-infra/var.tf
@@ -6,7 +6,7 @@ variable "public_subnet_cidr" { default = "10.0.1.0/24" }
 variable "instance_type" { default = "t3.micro" }
 variable "key_name" {
   description = "EC2 key pair name"
-  default     = "win-pwd"
+  default     = "ubuntu-static"
 }
 variable "allow_ssh_cidr" { default = "0.0.0.0/0" }
 variable "root_volume_size_gb" { default = 16 }
