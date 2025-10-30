e5b767a8b0c9e204b0b7d8b5c814d34f906ab44b
Jahnavi Onteddu <jahnavi.o21@iiits.in>
2025-10-30 16:12:01 +0530

    Update job-details.ts

--- jobportal-frontend/src/app/pages/applicant/jobs/job-details/job-details.ts
+++ jobportal-frontend/src/app/pages/applicant/jobs/job-details/job-details.ts
@@ -1,14 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-job-details',
+  standalone: true,
   imports: [],
   templateUrl: './job-details.html',
-  styleUrl: './job-details.css',
+  styleUrls: ['./job-details.css']
 })
-export class JobDetails implements OnInit{
+export class JobDetails implements OnInit {
   job: any;
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
@@ -16,10 +18,10 @@ export class JobDetails implements OnInit{
   ngOnInit(): void {
     const jobId = this.route.snapshot.paramMap.get('id');
     if (jobId) {
-      this.http.get(`http://localhost:8080/api/jobs/${jobId}`)
+      this.http.get(`${environment.apiUrl}/jobs/${jobId}`)
         .subscribe({
           next: (data) => this.job = data,
-          error: (err) => console.error('Error loading job details:', err)
+          error: (err) => console.error('❌ Error loading job details:', err)
         });
     }
   }
