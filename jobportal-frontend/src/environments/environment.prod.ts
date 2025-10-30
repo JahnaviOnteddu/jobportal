51bf018016cf987cf6655030cbbee9559c83e330
Jahnavi Onteddu <jahnavi.o21@iiits.in>
2025-10-30 15:22:56 +0530

    Update environment.prod.ts

--- jobportal-frontend/src/environments/environment.prod.ts
+++ jobportal-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://backend:8080/api'
+  apiBaseUrl: 'http://backend:8080/api',
+  apiUrl: 'http://backend:8080/api'    // 👈 add this line
 };
